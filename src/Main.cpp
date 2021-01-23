ae1e2df461986d31a17e018e36de57d55d061543
WyattH99 <WyattHansen99@gmail.com>
2021-01-23 17:17:19 -0600

    Brainstorming (ANSI Escape Charaters is the way to move foward)

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,11 +10,13 @@ using namespace std;
 
 // Start of Program
 int main(){
-    cout << "Helloworld" << endl;
+    cout << "\e[31m" << "Hello" << "\e[0m" << "World" << endl; // Testing ANSI Escape Codes
 
-    class Game* Game = new class Game();
-    Game->setNum(0);
-    cout << Game->getNum() << endl;
+    // class Game* GamePtr = new class Game();
+    // GamePtr->setNum(0);
+    // cout << GamePtr->getNum() << endl;
+    // GamePtr->setNum(5);
+    // cout << GamePtr->getNum() << endl;
 
     return 0;
 }
